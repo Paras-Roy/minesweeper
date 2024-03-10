836c502964a0be448edb85a7bf7b687654eb3060
Paras Roy <parasandroy@gmail.com>
2024-03-11 03:18:53 +0530

    apostrophe fix

--- components/modals/GameLost.jsx
+++ components/modals/GameLost.jsx
@@ -9,7 +9,7 @@ export default function GameLost() {
             <div className='bg-black bg-opacity-70 text-white flex flex-col items-center p-10 rounded-xl w-5/6 max-w-lg'>
                 <GiSurprisedSkull className="text-8xl mb-4" />
                 <span className='text-5xl font-bold text-red-500 mb-6'>Game Over</span>
-                <p className="text-xl text-center mb-6">You've stumbled into a deadly trap!</p>
+                <p className="text-xl text-center mb-6">You&apos;ve stumbled into a deadly trap!</p>
                 <div className="flex justify-evenly w-full">
                     <button onClick={()=>window.location.reload()} className='text-xl px-4 py-2 bg-gray-700 text-white rounded-md hover:bg-blue-700 cursor-pointer'>Try Again</button>
                     <Link href="/" className='text-xl px-4 py-2 bg-gray-700 text-white rounded-md hover:bg-red-700'>Go Home</Link>
--- components/modals/GameWon.jsx
+++ components/modals/GameWon.jsx
@@ -8,7 +8,7 @@ export default function GameWon({time}) {
             <div className='bg-white bg-opacity-80 text-white flex flex-col items-center p-10 rounded-xl w-5/6 max-w-lg'>
                 <FaTrophy className="text-8xl text-yellow-500 mb-4" />
                 <span className='text-5xl font-bold text-green-500 mb-6'>You Won!</span>
-                <p className="text-xl text-center mb-6 text-black">You've successfully navigated the minefield in just {time} seconds!</p>
+                <p className="text-xl text-center mb-6 text-black">You&apos;ve successfully navigated the minefield in just {time} seconds!</p>
                 <div className="flex justify-evenly w-full">
                     <button onClick={()=>window.location.reload()} className='text-xl px-4 py-2 bg-gray-700 text-white rounded-md hover:bg-green-700 cursor-pointer'>Play Again</button>
                     <Link href="/" className='text-xl px-4 py-2 bg-gray-700 text-white rounded-md hover:bg-blue-700'>Go Home</Link>
